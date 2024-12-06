e855cccd5c0c5c46e986795c7bd128fa942dffc9
Katie Stahl <kathryn.stahl@nationwidechildrens.org>
2024-12-06 17:14:16 -0500

    fix: use better column types for data and read from json files to update tables

--- .github/scripts/update_synapse_tables.py
+++ .github/scripts/update_synapse_tables.py
@@ -1,16 +1,18 @@
 import csv
+import json
 import sys
 from synapseclient import Synapse, Table
 import os
+import pandas as pd
 
 
 # the paths detected as changes from the "Get changed files" job mapped to their corresponding synapse table ids
 PATHS_TO_IDS = {
-    "project/data/DataStandardOrTool.tsv": "syn63096833",
-    "project/data/DataSubstrate.tsv": "syn63096834",
-    "project/data/DataTopic.tsv": "syn63096835",
-    "project/data/Organization.tsv": "syn63096836",
-    "project/data/UseCase.tsv": "syn63096837",
+    "project/data/DataStandardOrTool.json": "syn63096833",
+    "project/data/DataSubstrate.json": "syn63096834",
+    "project/data/DataTopic.json": "syn63096835",
+    "project/data/Organization.json": "syn63096836",
+    "project/data/UseCase.json": "syn63096837",
 }
 
 
@@ -25,31 +27,23 @@ def delete_table_rows(syn: Synapse, table_id: str) -> None:
     print("Finished deleting rows")
 
 
-def get_rows_from_tsv(file_path: str):
-    """Read a TSV file and return a list of lists, where each inner list represents a row.
-    NOTE: The first row of the file is expected to be the headers, which is skipped
-    :param file_path: path to tsv file
-    :return: list of lists representing rows of the tsv file"""
-    rows = []
-    with open(file_path, mode="r", newline="", encoding="utf-8") as tsv_file:
-        reader = csv.reader(tsv_file, delimiter="\t")
-        # Skip the header row
-        next(reader)
-        for row in reader:
-            rows.append(row)
-    return rows
-
-
 def populate_table(syn: Synapse, update_file: str, table_id: str) -> None:
     """Populate the table with updated data
     :param syn: synapse client
-    :param update_file: path for tsv file containing data to populate the table
+    :param update_file: path for json file containing data to populate the table
     :param table_id: synapse id for table to populate
     """
-    rows_to_add = get_rows_from_tsv(update_file)
-    print(f"Populating table for file: {update_file}")
-    table = syn.store(Table(table_id, rows_to_add))
-    print("Finished populating table")
+    with open(update_file, "r") as file:
+        data = json.load(file)
+    # each json file begins with a key that maps to the list of records, so we're accessing that list here
+    data = next(iter(data.values()), [])
+    if isinstance(data, list):
+        df = pd.DataFrame(data=data)
+        print(f"Populating table for file: {update_file}")
+        table = syn.store(Table(table_id, df))
+        print("Finished populating table")
+    else:
+        print("Could not get list of data from json file")
 
 
 def main():
