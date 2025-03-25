7afdd7b23f26addd3ab36963a0f7a231528e5534
Jennifer Bowser <jennifer.bowser@nationwidechildrens.org>
2025-03-25 11:46:23 -0400

    clean up test logic

--- tests/test_slot_registration.py
+++ tests/test_slot_registration.py
@@ -4,18 +4,15 @@ import subprocess
 import unittest
 from scripts.modify_synapse_schema import ColumnName, COLUMN_TEMPLATES, TableSchema
 
-DATA_DIRECTORY = "src/data/"
-MODIFIED_SYNAPSE_SCHEMA_FILE = "scripts/modify_synapse_schema.py"
-
-
 class TestSlotRegistration(unittest.TestCase):
+	DATA_DIRECTORY = "src/data/"
+	MODIFY_SYNAPSE_SCHEMA_FILE = "scripts/modify_synapse_schema.py"
 
 	@staticmethod
 	def _get_modified_data_files():
 		"""Retrieve a list of modified data files."""
 		diff = subprocess.run(["git", "diff", "--name-only", "origin/main"], capture_output=True, text=True)
-		modified_data_files = [file for file in diff.stdout.splitlines() if file.endswith(".yaml") and DATA_DIRECTORY in file]
-		return modified_data_files
+		return [file for file in diff.stdout.splitlines() if file.endswith(".yaml") and TestSlotRegistration.DATA_DIRECTORY in file]
 
 
 	@staticmethod
@@ -28,7 +25,7 @@ class TestSlotRegistration(unittest.TestCase):
 
 		for line in modified_lines:
 			if line.startswith("+") and not line.startswith("+++"):  # New/modified lines
-				# Lines with new/modified slots will start with a '+', then 2-4 spaces, then the slot name, then a colon
+				# Lines with new/modified slots will start with a '+', then 2-4 spaces, then thegitslot name, then a colon
 				match = re.match(r"^\+ {2,4}(\w+):", line)
 				slot_name = match.group(1) if match else None
 				if slot_name:
@@ -37,32 +34,34 @@ class TestSlotRegistration(unittest.TestCase):
 		return modified_slots
 
 
-	def test_slot_registration(self):
-		# PART ONE: Determine which (if any) data files contain slots that were added or modified,
-		# and capture those slots + the tables they live in
+	@staticmethod
+	def _get_tables_with_modified_slots():
+		"""Returns a dictionary containing tables with added/modified slots (keys), and a list of those slots (values)"""
 		modified_data_files = TestSlotRegistration._get_modified_data_files()
-		files_with_modified_slots = {}
-		modified_slot_set = set()
+		tables_with_modified_slots = {}
 
 		for file in modified_data_files:
-			modified_slots = TestSlotRegistration._get_modified_slots(file)
 			table_name = Path(file).stem
-			files_with_modified_slots[table_name] = modified_slots
-			modified_slot_set.update(modified_slots)
+			modified_slots = TestSlotRegistration._get_modified_slots(file)
+			tables_with_modified_slots[table_name] = modified_slots
+
+		return tables_with_modified_slots
 
 
-		# PART TWO: Verify that each slot has been added to all column definitions in 'modify_snyapse_schema.py'
-		# This includes the enums 'ColumnName' and 'TableSchema', plus the dictionary 'COLUMN_TEMPLATES'
+	def test_slot_registration(self):
+		"""
+		Verify that each added/modified slot has been added to all column definitions in 'modify_snyapse_schema.py'.
+		This includes the enums `ColumnName` and `TableSchema`, plus the dictionary `COLUMN_TEMPLATES`
+		"""
+		tables_with_modified_slots = TestSlotRegistration._get_tables_with_modified_slots()
+		for table, modified_slots in tables_with_modified_slots.items():
+			for slot in modified_slots:
+				# All slots should be registered in these places
+				self.assertTrue(slot in ColumnName, f"'{slot}' not in '{TestSlotRegistration.MODIFY_SYNAPSE_SCHEMA_FILE}' > 'ColumnName'")
+				self.assertTrue(ColumnName(slot) in list(COLUMN_TEMPLATES.keys()), f"'{slot}' not in '{TestSlotRegistration.MODIFY_SYNAPSE_SCHEMA_FILE}' > 'COLUMN_TEMPLATES'")
 
-		# All slots should be registered in these places
-		for slot in modified_slot_set:
-			self.assertTrue(slot in ColumnName, f"'{slot}' not in '{MODIFIED_SYNAPSE_SCHEMA_FILE}' > 'ColumnName'")
-			self.assertTrue(ColumnName(slot) in list(COLUMN_TEMPLATES.keys()), f"'{slot}' not in '{MODIFIED_SYNAPSE_SCHEMA_FILE}' > 'COLUMN_TEMPLATES'")
-
-		# Table-specific slot registration
-		for table, modified_table_slots in files_with_modified_slots.items():
-			for slot in modified_table_slots:
-				self.assertTrue(ColumnName(slot) in TableSchema[table].value["columns"], f"'{slot}' not in '{MODIFIED_SYNAPSE_SCHEMA_FILE}' > 'TableSchema[{table}]'")
+				# Table-specific slot registration
+				self.assertTrue(ColumnName(slot) in TableSchema[table].value["columns"], f"'{slot}' not in '{TestSlotRegistration.MODIFY_SYNAPSE_SCHEMA_FILE}' > 'TableSchema[{table}]'")
 
 
 if __name__ == "__main__":
