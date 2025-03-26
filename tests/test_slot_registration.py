0e10604a45f220847cad63dc8828ce2c76d83556
Jennifer Bowser <jennifer.bowser@nationwidechildrens.org>
2025-03-26 15:24:01 -0400

    adds tests for 'test_slot_registration'

--- tests/test_slot_registration.py
+++ tests/test_slot_registration.py
@@ -1,7 +1,10 @@
+from enum import Enum
 from pathlib import Path
 import unittest
+from unittest.mock import patch
 import yaml
-from scripts.modify_synapse_schema import ColumnName, COLUMN_TEMPLATES, TableSchema
+# from scripts.modify_synapse_schema import ColumnName, COLUMN_TEMPLATES, TableSchema
+import scripts.modify_synapse_schema as modify_synapse_schema
 
 class TestSlotRegistration(unittest.TestCase):
 	SCHEMA_DIRECTORY = "src/schema"
@@ -44,12 +47,82 @@ class TestSlotRegistration(unittest.TestCase):
 		for table, slots in table_slots.items():
 			for slot in slots:
 				# All slots should be registered in these places
-				self.assertTrue(slot in [entry.value for entry in ColumnName], f"'{slot}' not in '{TestSlotRegistration.MODIFY_SYNAPSE_SCHEMA_FILE}' > 'ColumnName'")
-				self.assertTrue(ColumnName(slot) in list(COLUMN_TEMPLATES.keys()), f"'{slot}' not in '{TestSlotRegistration.MODIFY_SYNAPSE_SCHEMA_FILE}' > 'COLUMN_TEMPLATES'")
+				self.assertTrue(
+					slot in [entry.value for entry in modify_synapse_schema.ColumnName],
+					f"'{slot}' not in '{TestSlotRegistration.MODIFY_SYNAPSE_SCHEMA_FILE}' > 'ColumnName'"
+				)
+				self.assertTrue(
+					modify_synapse_schema.ColumnName(slot) in list(modify_synapse_schema.COLUMN_TEMPLATES.keys()),
+					f"'{slot}' not in '{TestSlotRegistration.MODIFY_SYNAPSE_SCHEMA_FILE}' > 'COLUMN_TEMPLATES'"
+				)
 
 				# Table-specific slot registration
-				self.assertTrue(ColumnName(slot) in TableSchema[table].value["columns"], f"'{slot}' not in '{TestSlotRegistration.MODIFY_SYNAPSE_SCHEMA_FILE}' > 'TableSchema[{table}]'")
+				self.assertTrue(
+					modify_synapse_schema.ColumnName(slot) in modify_synapse_schema.TableSchema[table].value["columns"],
+					f"'{slot}' not in '{TestSlotRegistration.MODIFY_SYNAPSE_SCHEMA_FILE}' > 'TableSchema[{table}]'"
+				)
 
+
+
+class MockColumnName(Enum):
+	TEST_SLOT = "test_slot"
+
+MOCK_COLUMN_TEMPLATES = {
+	MockColumnName.TEST_SLOT: "Definition of column"
+}
+
+class MockTableSchema(Enum):
+	DataStandardOrTool = {
+		"id": "syn63096833",
+		"columns": [
+			MockColumnName.TEST_SLOT
+		],
+	}
+
+class TestSlotRegistrationTests(unittest.TestCase):
+	"""Tests for TestSlotRegistration.test_slot_registration()"""
+
+	def setUp(self):
+		table_slots = {"DataStandardOrTool": ['test_slot']}
+		self.table_slots_patcher = patch.object(TestSlotRegistration, '_get_table_slots', return_value=table_slots)
+		self.mock_table_slots = self.table_slots_patcher.start()
+		self.addCleanup(self.table_slots_patcher.stop)
+
+
+	def _run_target_test(self) -> unittest.TestResult:
+		test_case = TestSlotRegistration(methodName='test_slot_registration')
+		result = unittest.TestResult()
+		test_case.run(result)
+		return result
+
+	def test_unregistered_slot(self):
+		"""INVALID - Slot not defined anywhere"""
+		result = self._run_target_test()
+		self.assertEqual(len(result.failures), 1)
+
+
+	@patch("scripts.modify_synapse_schema.ColumnName", new=MockColumnName)
+	def test_slot_not_in_column_templates_or_table_schema(self):
+		"""INVALID - Slot not defined in 'COLUMN_TEMPLATES' or 'TableSchema'"""
+		result = self._run_target_test()
+		self.assertEqual(len(result.failures), 1)
+
+
+	@patch("scripts.modify_synapse_schema.ColumnName", new=MockColumnName)
+	@patch("scripts.modify_synapse_schema.COLUMN_TEMPLATES", new=MOCK_COLUMN_TEMPLATES)
+	def test_slot_not_in_table_schema(self):
+		"""INVALID - Slot not defined in 'TableSchema'"""
+		result = self._run_target_test()
+		self.assertEqual(len(result.failures), 1)
+
+
+	@patch("scripts.modify_synapse_schema.ColumnName", new=MockColumnName)
+	@patch("scripts.modify_synapse_schema.COLUMN_TEMPLATES", new=MOCK_COLUMN_TEMPLATES)
+	@patch("scripts.modify_synapse_schema.TableSchema", new=MockTableSchema)
+	def test_slot_is_registered(self):
+		"""VALID - Slot defined everywhere"""
+		result = self._run_target_test()
+		self.assertTrue(result.wasSuccessful())
 
 
 if __name__ == "__main__":
