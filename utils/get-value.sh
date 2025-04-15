a7ab3bbddec7398b8735b3b868c4d32152a9fc6f
Jennifer Bowser <jennifer.bowser@nationwidechildrens.org>
2025-04-15 10:30:50 -0400

    refactors 'get-value.sh' again to hopefully be more cross-interpreter compatible

--- utils/get-value.sh
+++ utils/get-value.sh
@@ -2,6 +2,9 @@
 # get the value of a key in the about.yaml file
 # https://stackoverflow.com/questions/1221833/pipe-output-and-capture-exit-status-in-bash
 
-set -o pipefail # ensures the exit status of the script is 1 (failure) if any part of the script fails
-
-grep ^$1: about.yaml | sed "s/$1:[[:space:]]//" || exit 1
+result=$(grep "^$1:" about.yaml)
+if [ $? -eq 0 ]; then
+  echo "$result" | sed "s/^$1:[[:space:]]*//"
+else
+  exit 1
+fi
